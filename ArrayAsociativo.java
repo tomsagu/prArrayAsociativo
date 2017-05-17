9479f77f0e9597aa1ba85b827e5e7a10000ace61
tomsagu <tsayago1997@gmail.com>
2017-05-17 19:24:22 +0200

    Test1

---
+++ ArrayAsociativo.java
@@ -0,0 +1,52 @@
+package prArrayAsociativo;
+
+import java.util.NoSuchElementException;
+
+public class ArrayAsociativo {
+	//Clases internas
+	private class Nodo{
+		private String clave;
+		private String valor;
+		private Nodo sig; //siguiente nodo
+		
+		public Nodo(String c, String v, Nodo s){
+			clave = c;
+			valor = v;
+			sig = s;
+		}
+	}
+	
+	//Variables de instancia
+	private Nodo primero;
+	
+	
+	//Constructores
+	public ArrayAsociativo(){
+		primero = null;
+	}
+	
+	public ArrayAsociativo(String[] claves, String[] valores){
+		this();
+		if (claves.length > 0){
+			primero = new Nodo(claves[0], valores[0],null);
+			for (int i = 1; i< claves.length; i++){
+				Nodo nuevo = new Nodo(claves[i], valores[i],primero);
+				primero = nuevo;
+			}
+		}
+	}
+	
+	//Métodos de instancia
+	public int size(){
+		int n = 0;
+		Nodo aux = primero;
+		while(aux != null){
+			n++;
+			aux = aux.sig;
+		}
+		return n;
+	}
+	
+	
+	
+}
---
+++ TestArrayAsociativo.java
@@ -0,0 +1,39 @@
+package prArrayAsociativo;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class TestArrayAsociativo {
+	private ArrayAsociativo a;
+	private String [] claves = {"nombre","apellido","dni","pais"};
+	private String [] valores = {"luis","martin","12345567P","España"};
+	
+	@Before
+	public void inicializar() {
+		a = new ArrayAsociativo();
+		System.out.println("Before");
+	}
+
+	@After
+	public void finalizar() {
+		a = null;
+		System.out.println("After");
+
+	}
+	
+	@Test
+	public void sizeArrayVacio() {
+		
+		assertTrue(0 == a.size());
+	
+	}
+	
+	@Test
+	public void sizeArrayNoVacio(){
+		a = new ArrayAsociativo(claves,valores);
+		assertTrue(4 == a.size());
+		a = null;
+	}
+
+}
